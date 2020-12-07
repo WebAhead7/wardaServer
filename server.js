73db2aab8810a0f49c85c46b65e9732a895a27a1
nizarhalloun <nshalloun@hotmail.com>
2020-12-07 12:52:09 +0200

    init

--- server.js
+++ server.js
@@ -14,13 +14,5 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.use('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  );
-}
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
